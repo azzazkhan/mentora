46f044790a430019be819813c0572c26d8ae1444
Azzaz Khan <hello@azzaz.dev>
2025-05-27 06:20:16 +0500

    wip: Linked Stripe customer object to local user

--- modules/User/database/seeders/UserSeeder.php
+++ modules/User/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Modules\User\Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Laravel\Cashier\Cashier;
 use Modules\Auth\Enums\Role;
 use Modules\User\Models\Teacher;
 
@@ -31,5 +32,24 @@ class UserSeeder extends Seeder
             ->each(fn(User $user) => $user->assignRole(Role::Teacher));
 
         User::factory()->count(50)->create()->each(fn(User $user) => $user->assignRole(Role::Student));
+    }
+
+    protected function deleteStripeCustomers(): void
+    {
+        if (! config('cashier.enabled')) {
+            return;
+        }
+
+        while (true) {
+            $customers = Cashier::stripe()->customers->all(['limit' => 100]);
+
+            foreach ($customers['data'] as $customer) {
+                Cashier::stripe()->customers->delete($customer['id']);
+            }
+
+            if (! $customers['has_more']) {
+                break;
+            }
+        }
     }
 }
